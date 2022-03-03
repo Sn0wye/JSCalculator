e7e75ae10394421132cdf7884e41ab74d3f95b4f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-03-03 12:29:54 -0300

    added scss

---
+++ main.js
@@ -0,0 +1,12 @@
+// Get all buttons OK
+const button = document.querySelectorAll("button")
+
+
+// add event
+button.addEventListener("click", addVisor);
+
+// Add visor
+function addVisor() {
+  console.log("the buttons are working");
+};
+
